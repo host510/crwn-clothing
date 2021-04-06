17d0d37a5aad4f496500e1691f7e85fa85dfa73a
Mikhail Gostishchev <>
2021-04-06 12:01:43 +0300

    redirect to home page if user signed in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -14,12 +14,11 @@ const Header = ({ currentUser }) => (
     <div className="options">
       <Link className="option" to="/shop">SHOP</Link>
       <Link className="option" to="/shop">CONTACT</Link>
-      {
-        currentUser ?
-          <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-          :
-          <Link className="option" to="/signin">SIGN IN</Link>
-      }
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+      ) : (
+        <Link className="option" to="/signin">SIGN IN</Link>
+      )}
     </div>
   </div>
 );
