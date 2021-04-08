e6cb011e7db810b3e90f7aec356dee42e0dd82fe
Mikhail Gostishchev <>
2021-04-08 17:12:08 +0300

    move directory and shop data into its own reducer, updated respective selectors and corresponding components with new redux flow for directory/shop

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-  <button className={`${inverted === true ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+  <button className={`${inverted === true ? 'inverted' : ''} ${isGoogleSignIn === true ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
     {children}
   </button>
 )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -15,7 +15,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
       <span className="subtitle">SHOP NOW</span>
     </div>
 
-  </div>
+  </div >
 )
 
 export default withRouter(MenuItem);
