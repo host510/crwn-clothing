6299b01a0cac3efd0a63b9533d213a3b01237a79
Mikhail Gostishchev <>
2021-04-09 16:19:05 +0300

    changed some components with styled-components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import './homepage.styles.scss';
 import Directory from '../../components/menu-item/directory/directory.component';
+import { HomePageContainer } from './homepage.styles';
 
 
 const HomePage = () => (
-  <div className="homepage">
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 )
 
 export default HomePage;
